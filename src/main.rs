bc3f5a9126a4579b3909ed123e7e2ea4097dc19a
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-09-07 07:21:58 -0400

    homework 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct Process {
+    instructions: String,
+    state: State,
+    input: String,
+    output: String,
+    memory: String,
+    priority: usize
+}
+
+enum State {
+    Running,
+    Ready,
+    Blocked
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
