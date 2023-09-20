e5044de7b67bca824f217a0c20c497b6838e766b
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-09-20 15:55:45 -0400

    added thraeds

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
+#![allow(dead_code, unused_variables)]
+
 struct Process {
-    instructions: String,
+    process_id: usize,
     state: State,
     input: String,
     output: String,
     memory: Vec<u8>,
     priority: usize,
+    threads: Vec<Thread>,
+}
+
+struct Thread {
+    instructions: String,
+    id: usize,
+    state: State,
+    process_id: usize,
+}
+
+impl Process {
+    fn create_thread(&mut self, id: usize, instructions: String, state: State) {
+        self.threads.push(Thread {
+            process_id: self.process_id,
+            id,
+            instructions,
+            state,
+        })
+    }
 }
 
 enum State {
@@ -15,11 +36,12 @@ enum State {
 
 fn main() {
     let process = Process {
-        instructions: String::from(""),
+        process_id: 0,
         state: State::Ready,
         input: String::from(""),
         output: String::from(""),
         memory: Vec::new(),
         priority: 0,
+        threads: vec![],
     };
 }
